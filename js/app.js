20fcc786d05238360c82bc44a709e9c6590042c0
KeddyB <bokelusi@gmail.com>
2023-12-20 13:29:45 +0100

    creating graph editor

--- js/app.js
+++ js/app.js
@@ -60,5 +60,12 @@ const s3 = new Segment(p3, p2);
 const s4 = new Segment(p1, p4);
 
 const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
+const graphEditor = new GraphEditor(myCanvas, graph)
 
-graph.draw(ctx);
+animate()
+
+function animate(){
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height)
+    graphEditor.display()
+    requestAnimationFrame(animate)
+}
