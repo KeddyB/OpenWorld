3a8302810ba5429d867e1f0f2ec737c11f1f7e0c
KeddyB <bokelusi@gmail.com>
2023-12-21 10:09:08 +0100

    progress

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -6,7 +6,7 @@ class Point{
     equals(point){
         return this.x == point.x && this.y == point.y
     }
-    draw(ctx, { size = 18, color = "black", outline = false } = {}){
+    draw(ctx, { size = 18, color = "black", outline = false, fill = false} = {}){
         const rad = size / 2
         ctx.beginPath()
         ctx.fillStyle = color
@@ -18,6 +18,12 @@ class Point{
             ctx.strokeStyle = "yellow"
             ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2)
             ctx.stroke()
+        }
+        if(fill){
+            ctx.beginPath()
+            ctx.arc(this.x, this.y, rad * 0.5, 0, Math.PI * 2)
+            ctx.fillStyle = "yellow"
+            ctx.fill()
         }
     }
 }
