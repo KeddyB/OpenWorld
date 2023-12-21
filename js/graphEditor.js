fd8355c3bbb016096862b040292bbeffffbcf051
KeddyB <bokelusi@gmail.com>
2023-12-21 09:36:16 +0100

    making progress

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -4,8 +4,25 @@ class GraphEditor{
         this.graph = graph
 
         this.ctx = this.canvas.getContext("2d")
+        this.selected = null
+        this.hovered = null
+        this.#addEventListeners()
+    }
+    #addEventListeners(){
+        this.canvas.addEventListener("mousedown", (evt)=>{
+            const mouse = new Point(evt.offsetX, evt.offsetY)
+            this.hovered = getNearestPoint(mouse, this.graph.points)
+            if(this.hovered){
+                this.selected= this.hovered
+            }
+            this.graph.addPoint(mouse)
+            this.selected = mouse
+        })
     }
     display(){
         this.graph.draw(this.ctx)
+        if(this.selected){
+            this.selected.draw(this.ctx, { outline: true})
+        }
     }
 }
