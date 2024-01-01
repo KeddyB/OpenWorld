57d0385d5fd69f4e8c001b27b5d2d53546674a62
KeddyB <bokelusi@gmail.com>
2024-01-01 15:02:55 +0100

    tree.js

--- js/world.js
+++ js/world.js
@@ -64,7 +64,7 @@ class World{
             }
             if(keep){
                 for (const tree of trees){
-                    if(distance(tree, p) < this.treeSize){
+                    if(distance(tree.center, p) < this.treeSize){
                         keep = false
                         break
                     }
@@ -153,7 +153,7 @@ class World{
             seg.draw(ctx, {color: "white", width: 4})
         }
         for(const tree of this.trees){
-            tree.draw(ctx, {size: this.treeSize, color: "rgba(0,0,0,.5"})
+            tree.draw(ctx)
         }
         for(const bld of this.buildings){
             bld.draw(ctx)
