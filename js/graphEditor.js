15820489f235522f2c4ed4d3066b3d8abf9c338c
KeddyB <bokelusi@gmail.com>
2024-01-03 13:34:44 +0100

    this is  a commit

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -15,18 +15,24 @@ class GraphEditor {
     }
     disable(){
         this.#removeEventListeners()
+        this.selected = false
+        this.hovered = false
     }
     #addEventListeners() {
-        this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this))
-        this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this))
-        this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault())
-        this.canvas.addEventListener("mouseup", () => { this.dragging = false })
+        this.boundMouseDown = this.#handleMouseDown.bind(this)
+        this.boundMouseMove = this.#handleMouseMove.bind(this)
+        this.boundMouseUp = () => { this.dragging = false }
+        this.boundContextMenu = (evt) => evt.preventDefault()
+        this.canvas.addEventListener("mousedown", this.boundMouseDown)
+        this.canvas.addEventListener("mousemove", this.boundMouseMove)
+        this.canvas.addEventListener("mouseup", this.boundMouseUp)
+        this.canvas.addEventListener("contextmenu", this.boundContextMenu)
     }
     #removeEventListeners() {
-        this.canvas.removeEventListener("mousedown", this.#handleMouseDown.bind(this))
-        this.canvas.removeEventListener("mousemove", this.#handleMouseMove.bind(this))
-        this.canvas.removeEventListener("contextmenu", (evt) => evt.preventDefault())
-        this.canvas.removeEventListener("mouseup", () => { this.dragging = false })
+        this.canvas.removeEventListener("mousedown", this.boundMouseDown)
+        this.canvas.removeEventListener("mousemove", this.boundMouseMove)
+        this.canvas.removeEventListener("mouseup", this.boundMouseUp)
+        this.canvas.removeEventListener("contextmenu", this.boundContextMenu)
     }
     #handleMouseDown(evt){
         if (evt.button == 2) { // right click
