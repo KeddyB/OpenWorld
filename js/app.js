2e7746fa3a29b0426f961326d87021b77f9bf055
KeddyB <bokelusi@gmail.com>
2024-01-05 10:23:11 +0100

    saving and loading the world

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const ctx = myCanvas.getContext("2d");
 
 const worldString = localStorage.getItem("world")
 const worldInfo = worldString ? JSON.parse(worldString) : null
-const world = worldInfo ? World.load(worldInfo) : new World(new Graph())
+let world = worldInfo ? World.load(worldInfo) : new World(new Graph())
 const graph = world.graph
 
 const viewport = new ViewPort(myCanvas)
@@ -95,7 +95,29 @@ function save() {
             encodeURIComponent(JSON.stringify(world))
     )
     const fileName = "name.world"
+    element.setAttribute("download", fileName)
+
+    element.click()
+
     localStorage.setItem("world", JSON.stringify(world))
+}
+function load(event){
+    const file = event.target.file[0]
+
+    if(!file){
+        alert("No file selected")
+        return
+    }
+    const reader = new FileReader()
+    reader.readAsText(file)
+
+    reader.onload = (evt) => {
+        const fileContent = evt.target.result
+        const jsonData = JSON.parse(fileContent);
+        world = World.load(jsonData)
+        localStorage.setItem("world", JSON.stringify(world))
+        location.reload()
+    }
 }
 function setMode(mode) {
     disableEditors()
